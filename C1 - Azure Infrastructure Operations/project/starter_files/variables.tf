10f0088c79cee7da95950d29b54f65188c2cd2c7
ShashankShekhar23 <86247520+ShashankShekhar23@users.noreply.github.com>
2021-06-25 00:04:25 +0530

    Updated variables.tf

--- C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
@@ -1,5 +1,6 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources"
+  default     = "project"
 }
 
 variable "location" {
@@ -7,10 +8,25 @@ variable "location" {
   default     = "East US"
 }
 
+variable "number" {
+  description = "The count of resources that should be created."
+  default     = "2"
+}
+
+variable "tags" {
+  description = "The tags which will be assigned to the resource created."
+  default     = {
+       Project_Name = "Deploying a Web Server in Azure"
+       Category         = "Web Server"
+}
+}
+
 variable "usr" {
   description = "The user name for the virtual machine that will be created."
+  default     = "adminuser"
 }
 
 variable "pwd" {
   description = "The password for the virtual machine that will be created."
+  default     = "Password@1234"
 }
